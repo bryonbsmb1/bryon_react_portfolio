57befc0e25f40cbf4df467137b97a522282d640a
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-28 11:47:39 -0500

    index.js added

---
+++ src/index2.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  About,
+  Contact,
+  Project,
+  Sidebar,
+  Resume
+} from "../components";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faEnvelope)
+
+ReactDOM.render(
+  <Router>
+    <Navigation />
+    <Sidebar />
+    <Routes>
+      <Route path="/" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+  </Router>,
+  document.getElementById('root')
+);
+reportWebVitals();
