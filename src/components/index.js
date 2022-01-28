5eb831c7c1b4699a9e1a5a728823509d87b745f6
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-27 22:33:38 -0500

    file order redone

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as About } from "./About";
 export { default as Contact } from "./Contact";
-export { default as Footer } from "./footer";
+export { default as Footer } from "./Footer";
 export { default as Navigation } from "./Navigation";
 export { default as Project } from "./Project";
 export { default as Sidebar } from "./Sidebar";
