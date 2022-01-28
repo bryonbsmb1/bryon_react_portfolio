0ed898087fe2009c076e31ebe9c6a6ebb596c0df
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-28 08:36:51 -0500

    deploy issues

--- components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as Footer } from "./footer";
-export { default as Navigation } from "./Navigation";
-export { default as Project } from "./Project";
-export { default as Sidebar } from "./Sidebar";
-export { default as Resume } from "./Resume";
